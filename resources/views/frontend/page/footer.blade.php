677e2fc8c967e8e2158a7b26162483ff482f45f2
kamrul1275 <kamrul1275.info@gmail.com>
2024-05-27 13:57:07 +0600

    social link add

--- resources/views/frontend/page/footer.blade.php
+++ resources/views/frontend/page/footer.blade.php
@@ -1,138 +1,134 @@
 <footer class="footer-section mt-5">
-    <div class="container">
-      <div class="footer-cta pt-3 pb-3">
-        <div class="row">
-          <div class="col-xl-4 col-md-4 mb-30">
-            <div class="single-cta">
-              <i class="fas fa-map-marker-alt text-danger"></i>
-              <div class="cta-text">
-                <h4>Find us</h4>
-                <span>Dhamrai,Dhaka 1350</span>
+        <div class="container">
+          <div class="footer-cta pt-3 pb-3">
+            <div class="row">
+              <div class="col-xl-4 col-md-4 mb-30">
+                <div class="single-cta">
+                  <i class="fas fa-map-marker-alt text-danger"></i>
+                  <div class="cta-text">
+                    <h4>Find us</h4>
+                    <span>Dhamrai,Dhaka 1350</span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-4 col-md-4 mb-30">
+                <div class="single-cta">
+                  <i class="fas fa-phone text-danger"></i>
+                  <div class="cta-text">
+                    <h4>Call us</h4>
+                    <span>+880-1307450464</span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-4 col-md-4 mb-30">
+                <div class="single-cta">
+                  <i class="far fa-envelope-open text-danger"></i>
+                  <div class="cta-text">
+                    <h4>Mail us</h4>
+                    <span>hellocoders.info@gmail.com</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div class="col-xl-4 col-md-4 mb-30">
-            <div class="single-cta">
-              <i class="fas fa-phone text-danger"></i>
-              <div class="cta-text">
-                <h4>Call us</h4>
-                <span>+880-1307450464</span>
+          <div class="footer-content pt-3 pb-3">
+            <div class="row">
+              <div class="col-xl-4 col-lg-4 mb-50">
+                <div class="footer-widget">
+                  <div class="footer-logo">
+                    <a
+                      style="
+                        font-size: 42px;
+                        color: #00a1c4;
+                        font-family: 'Lato', sans-serif;
+                      "
+                      href="index.html"
+                      >He<span style="color: #5f687b">//</span>o
+                      <span class="text-danger" style="font-weight: 700"
+                        >Coders BD</span
+                      ></a
+                    >
+                  </div>
+                  <div class="footer-text text-left">
+                    <p class="text-left">
+                      When you have software issues, we are here to help. We are
+                      commited to provide the finest possible solution.
+                    </p>
+                  </div>
+                  <div class="footer-social-icon for_disktop">
+                    <span>Follow us</span>
+                    <a href="https://www.facebook.com/Software.HelloCodersBd/"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                    <a href="https://www.linkedin.com/in/hello-coders-bd-38ba3522b/"><i class="fa-brands fa-linkedin-in twitter-bg"></i></a>
+                    <a href="https://www.youtube.com/@hellocodersbd7996"><i class="fa-brands fa-youtube google-bg"></i></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                <div class="footer-widget">
+                  <div class="footer-widget-heading">
+                    <h3>Useful Links</h3>
+                  </div>
+                  <ul>
+                    <li><a href="{{route('index.page')}}">Home</a></li>
+                    <li><a href="{{route('about.page')}}">About</a></li>
+                    <li><a href="{{route('service.page')}}">Services</a></li>
+                    <li><a href="#">Expert Team</a></li>
+                    <li><a href="{{route('career.page')}}">Career</a></li>
+                    <li><a href="{{route('contact.page')}}">Contact</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="footer-social-icon for_mobile">
+                <span>Follow us </span>
+                <a href="https://www.facebook.com/Software.HelloCodersBd/"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                <a href="https://www.linkedin.com/in/hello-coders-bd-38ba3522b/"><i class="fa-brands fa-linkedin-in twitter-bg"></i></a>
+                <a href="https://www.youtube.com/@hellocodersbd7996"><i class="fa-brands fa-youtube google-bg"></i></a>
+              </div>
+              <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                <div class="footer-widget">
+                  <div class="footer-widget-heading">
+                    <h3>Subscribe</h3>
+                  </div>
+                  <div class="footer-text mb-25">
+                    <p>
+                      Don’t miss to subscribe to our new feeds, kindly fill the
+                      form below.
+                    </p>
+                  </div>
+                  <div class="subscribe-form">
+                    <form action="#">
+                      <input type="text" placeholder="Email Address" />
+                      <button><i class="fab fa-telegram-plane"></i></button>
+                    </form>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div class="col-xl-4 col-md-4 mb-30">
-            <div class="single-cta">
-              <i class="far fa-envelope-open text-danger"></i>
-              <div class="cta-text">
-                <h4>Mail us</h4>
-                <span>hellocoders.info@gmail.com</span>
+        </div>
+        <div class="copyright-area">
+          <div class="container">
+            <div class="row">
+              <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+                <div class="copyright-text">
+                  <p>
+                    Copyright &copy; 2023, All Right Reserved
+                    <a class="text-danger copyright-area-name">Hello Coders BD</a>
+                  </p>
+                </div>
+              </div>
+              <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                <div class="footer-menu">
+                  <ul>
+                    <li><a href="{{route('index.page')}}">Home</a></li>
+                    <li><a href="#">Terms</a></li>
+                    <li><a href="#">Privacy</a></li>
+                    <li><a href="#">Policy</a></li>
+                    <li><a href="{{route('contact.page')}}">Contact</a></li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="footer-content pt-3 pb-3">
-        <div class="row">
-          <div class="col-xl-4 col-lg-4 mb-50">
-            <div class="footer-widget">
-              <div class="footer-logo">
-                <a
-                  style="
-                    font-size: 42px;
-                    color: #00a1c4;
-                    font-family: 'Lato', sans-serif;
-                  "
-                  href="index.html"
-                  >He<span style="color: #5f687b">//</span>o
-                  <span class="text-danger" style="font-weight: 700"
-                    >Coders BD</span
-                  ></a
-                >
-              </div>
-              <div class="footer-text text-left">
-                <p class="text-left">
-                  When you have software issues, we are here to help. We are
-                  commited to provide the finest possible solution.
-                </p>
-              </div>
-              <div class="footer-social-icon for_disktop">
-                <span>Follow us</span>
-                <a href="#"
-                  ><i class="fab fa-facebook-f facebook-bg"></i
-                ></a>
-                <a href="#" style="color: red;"><i class="fab fa-youtube fa-3x"></i></a>
-                <a href="#"
-                  ><i class="fab fa-google-plus-g google-bg"></i
-                ></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
-            <div class="footer-widget">
-              <div class="footer-widget-heading">
-                <h3>Useful Links</h3>
-              </div>
-              <ul>
-                <li><a href="{{route('index.page')}}">Home</a></li>
-                <li><a href="{{route('about.page')}}">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Expert Team</a></li>
-                <li><a href="{{route('all.career')}}">Career</a></li>
-                <li><a href="{{route('contact.page')}}">Contact</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="footer-social-icon for_mobile">
-            <span>Follow us</span>
-            <a href="#"><i class="fab fa-facebook-f facebook-bg"></i></a>
-            <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
-            <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
-          </div>
-          <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
-            <div class="footer-widget">
-              <div class="footer-widget-heading">
-                <h3>Subscribe</h3>
-              </div>
-              <div class="footer-text mb-25">
-                <p>
-                  Don’t miss to subscribe to our new feeds, kindly fill the
-                  form below.
-                </p>
-              </div>
-              <div class="subscribe-form">
-                <form action="#">
-                  <input type="text" placeholder="Email Address" />
-                  <button><i class="fab fa-telegram-plane"></i></button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="copyright-area">
-      <div class="container">
-        <div class="row">
-          <div class="col-xl-6 col-lg-6 text-center text-lg-left">
-            <div class="copyright-text">
-              <p>
-                Copyright &copy; 2023, All Right Reserved
-                <a class="text-danger copyright-area-name">Hello Coders BD</a>
-              </p>
-            </div>
-          </div>
-          <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
-            <div class="footer-menu">
-              <ul>
-                <li><a href="{{route('index.page')}}">Home</a></li>
-                <li><a href="#">Terms</a></li>
-                <li><a href="#">Privacy</a></li>
-                <li><a href="#">Policy</a></li>
-                <li><a href="{{route('contact.page')}}">Contact</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
+      </footer>
