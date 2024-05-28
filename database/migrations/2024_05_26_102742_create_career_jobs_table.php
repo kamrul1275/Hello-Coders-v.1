f2574919cfb5d19ecfcfb775bd535ad488a84c57
kamrul1275 <kamrul1275.info@gmail.com>
2024-05-28 10:15:52 +0600

    footer update

--- database/migrations/2024_05_26_102742_create_career_jobs_table.php
+++ database/migrations/2024_05_26_102742_create_career_jobs_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('career_jobs', function (Blueprint $table) {
             $table->id();
             $table->string('job_title');
-            $table->string('job_title');
             $table->string('job_position');
             $table->string('career_image');
             $table->timestamps();
